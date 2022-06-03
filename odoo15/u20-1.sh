94ed7b0d5e49640d8d271c92629543b237cd4882
Sezam <KMJY9rXVeHR2KvV>
2022-06-03 13:11:49 +0300

    Ubuntu 20.4.04 1st script

---
+++ odoo15/u20-1.sh
@@ -0,0 +1,12 @@
+apt install python3-dev libxml2-dev libxslt1-dev libldap2-dev libsasl2-dev libtiff5-dev libjpeg8-dev libopenjp2-7-dev zlib1g-dev libfreetype6-dev liblcms2-dev libwebp-dev libharfbuzz-dev libfribidi-dev libxcb1-dev libpq-dev
+apt install gcc -y
+mkdir -p /opt/odoo15
+chown -R $USER /opt/odoo15/
+apt install git -y
+git clone https://github.com/odoo/odoo -b 15.0 --depth=1 /opt/odoo15/odoo
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+cd ~/.pyenv && src/configure && make -C src
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+reboot
