263947ae798a10fe8cdee4a3be4b065b30627d51
Sezam <KMJY9rXVeHR2KvV>
2022-06-04 19:29:00 +0300

    Ubuntu 20.4.04 Pyenv

--- odoo15/u20-1.sh
+++ odoo15/u20-1.sh
@@ -1,4 +1,5 @@
-apt install python3-dev libxml2-dev libxslt1-dev libldap2-dev libsasl2-dev libtiff5-dev libjpeg8-dev libopenjp2-7-dev zlib1g-dev libfreetype6-dev liblcms2-dev libwebp-dev libharfbuzz-dev libfribidi-dev libxcb1-dev libpq-dev
+apt update
+apt install python3-dev libxml2-dev libxslt1-dev libldap2-dev libsasl2-dev libtiff5-dev libjpeg8-dev libopenjp2-7-dev zlib1g-dev libfreetype6-dev liblcms2-dev libwebp-dev libharfbuzz-dev libfribidi-dev libxcb1-dev libpq-dev -y
 apt install gcc -y
 mkdir -p /opt/odoo15
 chown -R $USER /opt/odoo15/
@@ -9,4 +10,4 @@ cd ~/.pyenv && src/configure && make -C src
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
 echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(pyenv init -)"' >> ~/.bashrc
-reboot
+echo 'reboot server'
