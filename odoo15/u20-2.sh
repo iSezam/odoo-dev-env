a2c7d252078c44afc85279e07964f5cab345b1e8
Sezam <KMJY9rXVeHR2KvV>
2022-06-04 20:50:09 +0300

    Ubuntu 20.4.04 inotify

--- odoo15/u20-2.sh
+++ odoo15/u20-2.sh
@@ -5,5 +5,6 @@ pyenv virtualenv 3.9.10 odoo15
 pyenv activate odoo15
 cd /opt/odoo15/odoo
 pip install setuptools wheel
-pip -r requirements.txt
+pip install -r requirements.txt
+pip install inotify
 pyenv deactivate odoo15
