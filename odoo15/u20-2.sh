43dfdf9cc27e57ba51914c93a686174b2577515c
Sezam <KMJY9rXVeHR2KvV>
2022-06-04 20:18:36 +0300

    Ubuntu 20.4.04 virtualenv

--- odoo15/u20-2.sh
+++ odoo15/u20-2.sh
@@ -1,3 +1,5 @@
+git clone https://github.com/pyenv/pyenv-virtualenv.git $(pyenv root)/plugins/pyenv-virtualenv
+echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
 pyenv install -v 3.9.10
 pyenv virtualenv 3.9.10 odoo15
 pyenv activate odoo15
